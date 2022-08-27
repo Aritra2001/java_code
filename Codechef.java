2b09e38753fbe4f1eabb23f1b59f0c61b97f5e28
Aritra2001 <90918557+Aritra2001@users.noreply.github.com>
2022-08-27 20:02:36 +0530

    Add files via upload

---
+++ Codechef.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		for(int i = 0 ; i < t ; i++)
+		{
+		    String S = br.readLine();
+		    String T = br.readLine();
+		    String M = "";
+		    for(int j = 0 ; j < 5 ; j++)
+		    {
+		        
+		        if(S.charAt(j) == T.charAt(j))
+		        M = M + 'G';
+		        else
+		        M = M + 'B';
+		    }
+		    System.out.println(M);
+		}
+		
+	}
+}
