304306ddd06ad2cb7d04d482b709d7c60119d766
Aritra2001 <90918557+Aritra2001@users.noreply.github.com>
2022-09-05 19:23:12 +0530

    Add files via upload

--- Abstract.java
+++ Abstract.java
@@ -1,5 +1,6 @@
-//Use of abstact class to find the area 
-//of a sqaure
+//Use of abstact class to find  
+//the area of a sqaure
+//the area of Right angled traingle
 abstract class Fig
 {
     int l1;
@@ -22,6 +23,20 @@ class Square extends Fig
         return sq_area;
     }
 }
+class Traingle extends Fig
+{
+    int tr_area , b1;
+    Traingle(int a , int b)
+    {
+        super(a);
+        b1 = b;
+    }
+    int Area()
+    {
+        tr_area = (int)(l1 * b1)/2;
+        return tr_area;
+    }
+}
 class Abstract
 {
     public static void main(String args[])
@@ -29,5 +44,8 @@ class Abstract
         Square obj = new Square(4);
         int p = obj.Area();
         System.out.println("The area of square is :"+p);
+        Traingle ob = new Traingle(4,5);
+        int q = ob.Area();
+        System.out.println("The area of Traingle is :"+q);
     }
 }
